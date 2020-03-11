4703e9d74b0e13765277a059ee7996cd20ae321e
YinChang <yincahng0626@gmail.com>
2020-03-11 09:42:02 +0800

    add FS.Abp.Npoi.Mapper

---
+++ src/FS.Abp.Npoi.Mapper/FS.Abp.Npoi.Mapper/ExportFactory.cs
@@ -0,0 +1,35 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FS.Abp.Npoi.Mapper
+{
+    public interface IExportFactory:Volo.Abp.DependencyInjection.ITransientDependency
+    {
+        byte[] Produce<T>(string sheetName, IEnumerable<T> modelJson)
+            where T : class;
+    }
+    public class ExportFactory : IExportFactory
+    {
+        public byte[] Produce<T>(string sheetName, IEnumerable<T> modelJson)
+            where T:class
+        {
+            IWorkbook workbook = new XSSFWorkbook();
+            ISheet sheet = workbook.CreateSheet(sheetName);
+            var mapper = new global::Npoi.Mapper.Mapper(workbook);
+            mapper.ForHeader(cell =>
+            {
+                sheet.AutoSizeColumn(cell.ColumnIndex);
+            });
+            mapper.Put<T>(modelJson, 0);
+            MemoryStream memoryStream = new MemoryStream();
+            mapper.Workbook.Write(memoryStream);
+            var datas = memoryStream.ToArray();
+            memoryStream.Close();
+            return datas;
+        }
+    }
+}
