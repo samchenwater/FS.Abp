b786bf1ef78bc181774ad3b3fd1fb5cd7dd7f6ac
YinChang <yincahng0626@gmail.com>
2020-03-11 14:56:38 +0800

    add FS.Abp.Host.Shared

---
+++ src/FS.Abp.Host.Shared/Volo.Abp.FeatureManagement/AbpFeatureManagementHttpApiHostModule.cs
@@ -0,0 +1,17 @@
+﻿using Volo.Abp.FeatureManagement.EntityFrameworkCore;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.FeatureManagement.HttpApi
+{
+    [DependsOn(
+        typeof(AbpFeatureManagementApplicationModule),
+        typeof(AbpFeatureManagementHttpApiModule),
+        typeof(AbpFeatureManagementEntityFrameworkCoreModule)
+        )]
+    public class AbpFeatureManagementHttpApiHostModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+        }
+    }
+}
